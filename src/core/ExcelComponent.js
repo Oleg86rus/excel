64022380f3238e00539ad0dc06b218506176c8d0
oleg086rus <o.pototskiy@yandex.ru>
2022-11-16 00:54:17 +0500

    some in emit

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,9 +5,11 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners)
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.unsubscribers = []
     this.prepare()
   }
 
+  // настраиваем компонент до init
   prepare() {
 
   }
@@ -17,11 +19,27 @@ export class ExcelComponent extends DomListener {
     return ''
   }
 
+  // уведомляем слушателей про событие event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // подписываемся на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
+
+  // инициализируем компонент
+  // добавляем DOM слушателей
   init() {
     this.initDomListeners()
   }
 
+  // удаляем компонент
+  // чистим слушатели
   destroy() {
     this.removeDomListeners()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
