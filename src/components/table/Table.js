3c7c27c8d3bf040f1a93aa5607abd1116c91ad0a
oleg086rus <o.pototskiy@yandex.ru>
2022-11-15 20:22:47 +0500

    selection

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,6 +2,7 @@ import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
 import {resizeHandler} from '@/components/table/table.resize'
 import {shouldResize} from '@/components/table/table.functions'
+import {TableSelection} from '@/components/table/TableSelection'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -9,12 +10,21 @@ export class Table extends ExcelComponent {
   constructor($root) {
     super($root, {
       listeners: ['mousedown']
-
     })
   }
 
   toHTML() {
     return createTable(100)
+  }
+
+  prepare() {
+    this.selection = new TableSelection()
+  }
+
+  init() {
+    super.init()
+    const $cell = this.$root.find('[data-id="0:0"]')
+    this.selection.select($cell)
   }
 
   onMousedown(event) {
--- src/core/dom.js
+++ src/core/dom.js
@@ -26,6 +26,10 @@ class Dom {
     this.$el.removeEventListener(eventType, callback)
   }
 
+  find(selector) {
+    return $(this.$el.querySelector(selector))
+  }
+
   append(node) {
     if (node instanceof Dom) {
       node = node.$el
@@ -56,6 +60,14 @@ class Dom {
 
   css(styles = {}) {
     Object.keys(styles).forEach(key => this.$el.style[key] = styles[key])
+  }
+
+  addCless(className) {
+    this.$el.classList.add(className)
+  }
+
+  removeCless(className) {
+    this.$el.classList.remove(className)
   }
 }
 
